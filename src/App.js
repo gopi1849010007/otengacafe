cbaccf36d9d5846b232e68e9fbd441406db5c5c3
Gopi Nishaad <kumargopi046@gmail.com>
2023-08-30 20:42:41 +0530

    componentcreated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 
 export default function App() {
-  return <h1>Hello World I am Gopi Learning React</h1>
+  return (<div>
+  <Hello/>
+  <Mentor/>
+  </div>)
+  
+}
+
+function Hello() {
+  return(<div><h1> I am helo component</h1></div>)
+  
+}
+
+
+function Mentor() {
+  return(<div><h3>My mentor name is siddharth vishen.</h3></div>)
   
 }
